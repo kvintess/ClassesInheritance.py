ab71644ae5e69c4000136a42f384c4cd7589f9a8
kvintessentsia <trinitikvintessentsia@yandex.ru>
2022-09-28 12:56:10 +0300

    2 next commit

--- MoredifficultProject.py
+++ MoredifficultProject.py
@@ -1,6 +1,7 @@
 #модель доставки грузов
 
 class Road:
+
     def init(self, start, end, distance):
         self.start = start
         self.end = end
@@ -23,7 +24,7 @@ class Warehouse:
     def get_next_truck(self):
         pass
 
-    def truck_ready(self):
+    def truck_ready(self, truck):
         pass
 
     def act(self):
@@ -38,6 +39,17 @@ class Vehicle:
     def __str__(self):
         pass
 
+    def tank_up(self):
+        pass
+
+class Truck(Vehicle):
+
+    def __init__(self, model, body_space=1000):
+        pass
+
+    def __str__(self):
+        pass
+
     def ride(self):
         pass
 
@@ -46,3 +58,44 @@ class Vehicle:
 
     def act(self):
         pass
+
+class AutoLoader(Vehicle):
+
+    def __init__(self, model,bucket_capacity=100, warehouse=None, role='loader'):
+        pass
+
+    def __str__(self):
+        pass
+
+    def act(self):
+        pass
+
+    def load(self):
+        pass
+
+    def unload(self):
+        pass
+
+TOTAL_CARGO = 100000
+
+moscow = Warehouse(name='Москва',content=TOTAL_CARGO)
+piter = Warehouse(name='Питер',content=0)
+
+moscow_piter = Road(start=moscow, end=piter, distanse=715)
+piter_moscow = Road(start=piter, end=moscow, distanse=780)
+
+moscow.set_road_out(moscow_piter)
+piter.set_road_out(piter_moscow)
+
+loader_1 = AutoLoader(model='Bobcat', bucket_capacity=1000, warehouse=moscow
+                      role='loader')
+loader_2 = AutoLoader(model='lonking', bucket_capacity=500, warehouse=piter
+                      role='unloader')
+
+truck_1 = Truck(model='Камаз', body_space=5000)
+truck_2 = Truck(model='Газ', body_space=2000)
+
+moscow.truck_arrived(truck_1)
+moscow.truck_arrived(truck_2)
+
+hour = 0
